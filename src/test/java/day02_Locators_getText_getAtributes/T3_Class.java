6ae5e65cc61c9b8691c47b54a1511e564cc03c6d
Farid <ferid2805@gmail.com>
2022-12-30 15:14:03 +0200

    T3_googleSearch

--- src/test/java/day02_Locators_getText_getAtributes/T3_Class.java
+++ src/test/java/day02_Locators_getText_getAtributes/T3_Class.java
@@ -2,33 +2,31 @@ package day02_Locators_getText_getAtributes;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class T3_googleSearch {
-    public static void main(String[] args) {
+public class T3_Class {
+
+        public static void main(String[] args) {
 /*
 task----Google Search
-
 Open a chrome browser
 go to : https://www.google.com
 write apple in a search box
 click google search button
 verify title
 Expected: title should start with "apple" word
-
  */
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver= new ChromeDriver();
-        driver.manage().window().maximize();
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver= new ChromeDriver();
+            driver.manage().window().maximize();
 
-        driver.get("https://www.google.com");
-        WebElement googleSearchBox =driver.findElement(By.name("q"));
-        googleSearchBox.sendKeys("apple");
+            driver.get("https://www.google.com");
+            WebElement googleSearchBox =driver.findElement(By.name("q"));
+            googleSearchBox.sendKeys("apple"+ Keys.ENTER);
 
 
-
-    }
-}
+        }
