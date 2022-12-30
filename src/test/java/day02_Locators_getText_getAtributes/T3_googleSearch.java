1dd80668b7ee9422b576caa11a3c4f3832a05145
Farid <ferid2805@gmail.com>
2022-12-30 14:49:52 +0200

    T3_googleSearch

--- src/test/java/day02_Locators_getText_getAtributes/T3_googleSearch.java
+++ src/test/java/day02_Locators_getText_getAtributes/T3_googleSearch.java
@@ -1,7 +1,10 @@
 package day02_Locators_getText_getAtributes;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T3_googleSearch {
@@ -23,6 +26,9 @@ Expected: title should start with "apple" word
         driver.manage().window().maximize();
 
         driver.get("https://www.google.com");
+        WebElement googleSearchBox =driver.findElement(By.name("q"));
+        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+
 
 
     }
