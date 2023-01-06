d63f7b11d69de1941b537ea2f003530cbcca3e53
Farid <ferid2805@gmail.com>
2023-01-06 15:50:04 +0200

    T1 xPath And Css  Practices

---
+++ src/test/java/day04_findElements/FindElements.java
@@ -0,0 +1,7 @@
+package day04_findElements;
+
+public class FindElements {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/day04_findElements/xPathAndCssPractices.java
+++ src/test/java/day04_findElements/xPathAndCssPractices.java
@@ -6,11 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class xPathAndCssPractices {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get("https://practice.cydeo.com/forgot_password");
         WebElement homeLink= driver.findElement(By.cssSelector("a[class='nav-link']"));
