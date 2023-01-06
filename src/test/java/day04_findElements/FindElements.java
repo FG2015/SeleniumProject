e2b0eaa8ec9d89e71f1cbc72f66a7af0c8dd65f3
Farid <ferid2805@gmail.com>
2023-01-06 16:13:49 +0200

    T2 find elements practices

--- src/test/java/day04_findElements/FindElements.java
+++ src/test/java/day04_findElements/FindElements.java
@@ -1,7 +1,29 @@
 package day04_findElements;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class FindElements {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://practice.cydeo.com/abtest");
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        System.out.println("allLinks.size() = " + allLinks.size());
+
+        for (WebElement each : allLinks) {
+            System.out.println("text of link = " + each.getText());
+            System.out.println("get href value = " + each.getAttribute("href"));
+        }
 
     }
 }
