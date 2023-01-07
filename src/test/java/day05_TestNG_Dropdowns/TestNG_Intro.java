78e7e8e6fac510b077d8d8536ac8e071ba46a398
Farid <ferid2805@gmail.com>
2023-01-08 00:09:00 +0200

    Test NG intro

---
+++ src/test/java/day05_TestNG_Dropdowns/TestNG_Intro.java
@@ -0,0 +1,11 @@
+package day05_TestNG_Dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+    @Test
+    public void Test1(){
+        System.out.println("TEst 1 is running ");
+
+    }
+}
