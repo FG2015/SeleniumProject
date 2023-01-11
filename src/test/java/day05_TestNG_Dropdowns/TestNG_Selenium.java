17d1541b705873349cb331e9d9118768666d6cb2
Farid <ferid2805@gmail.com>
2023-01-11 19:07:30 +0200

    Test NG  and selenium

--- src/test/java/day05_TestNG_Dropdowns/TestNG_Selenium.java
+++ src/test/java/day05_TestNG_Dropdowns/TestNG_Selenium.java
@@ -3,6 +3,7 @@ package day05_TestNG_Dropdowns;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -16,5 +17,6 @@ public class TestNG_Selenium {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://google.com");
         //Assert title is Google
+        Assert.assertEquals(driver.getTitle(),"Google");
     }
 }
