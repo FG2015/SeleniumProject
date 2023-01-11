c5ae22fd82ca7dfc860053d1623275d978cabfd5
Farid <ferid2805@gmail.com>
2023-01-11 18:54:45 +0200

    Test NG  and selenium

--- src/test/java/day05_TestNG_Dropdowns/TestNG_Selenium.java
+++ src/test/java/day05_TestNG_Dropdowns/TestNG_Selenium.java
@@ -1,4 +1,20 @@
 package day05_TestNG_Dropdowns;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class TestNG_Selenium {
+    @Test
+    public void selenium_test(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+        //Assert title is Google
+    }
 }
