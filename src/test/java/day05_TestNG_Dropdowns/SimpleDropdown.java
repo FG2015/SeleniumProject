990c9f32dbe16405a801ea87c74dd041331d890b
Farid <ferid2805@gmail.com>
2023-01-12 14:11:24 +0200

    Dropdowns

--- src/test/java/day05_TestNG_Dropdowns/SimpleDropdown.java
+++ src/test/java/day05_TestNG_Dropdowns/SimpleDropdown.java
@@ -3,30 +3,36 @@ package day05_TestNG_Dropdowns;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class SimpleDropdown {
-    WebDriver driver;
-    @BeforeMethod
-    public  void setUpMethod(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://practice.cydeo.com/dropdown");
-    }
 
 
 
 
     @Test
     public void simpleDropDownTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
        Select simpleDropdown= new Select(driver.findElement(By.cssSelector("select[id='dropdown']")));
+        WebElement currentlySelectedOptıon= simpleDropdown.getFirstSelectedOption();
+        String actualSimpleDropdownTest= currentlySelectedOptıon.getText();
+        System.out.println(actualSimpleDropdownTest);
+
+
+        Select stateDropDown= new Select(driver.findElement(By.cssSelector("select[id='state']")));
+
+
+
+
 
     }
 }
