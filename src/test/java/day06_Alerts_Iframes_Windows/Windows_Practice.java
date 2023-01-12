5b8c097dee8954984961222bfa2ce996ea9eaa6d
Farid <ferid2805@gmail.com>
2023-01-12 19:50:18 +0200

    day06_ Windows_Practice

---
+++ src/test/java/day06_Alerts_Iframes_Windows/Windows_Practice.java
@@ -0,0 +1,19 @@
+package day06_Alerts_Iframes_Windows;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Windows_Practice {
+    @Test
+    public void windowsTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/windows");
+    }
+}
