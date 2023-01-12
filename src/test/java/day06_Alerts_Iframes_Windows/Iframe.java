b8b67a2ca88e44bee0ead0f3c565332be32aecff
Farid <ferid2805@gmail.com>
2023-01-12 18:57:30 +0200

    day06_ Iframe task

---
+++ src/test/java/day06_Alerts_Iframes_Windows/Iframe.java
@@ -0,0 +1,26 @@
+package day06_Alerts_Iframes_Windows;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Iframe {
+    @Test
+    public void IframeTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/iframe");
+        driver.switchTo().frame("mce_0_ifr");
+
+        WebElement YourContentGoesHere= driver.findElement(By.xpath("//p"));
+        Assert.assertTrue(YourContentGoesHere.isDisplayed());
+    }
+}
