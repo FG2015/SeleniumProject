2975bd1370e27c0b0c2562940727bf99e6e6a4a6
Farid <ferid2805@gmail.com>
2023-01-12 20:11:01 +0200

    day06_ Windows_Practice

--- src/test/java/day06_Alerts_Iframes_Windows/Windows_Practice.java
+++ src/test/java/day06_Alerts_Iframes_Windows/Windows_Practice.java
@@ -1,7 +1,9 @@
 package day06_Alerts_Iframes_Windows;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -24,6 +26,14 @@ public class Windows_Practice {
         String expectedTitle="Windows";
         String actualTitle=driver.getTitle();
         Assert.assertEquals(actualTitle,expectedTitle);
+
+        WebElement clickHereButton= driver.findElement(By.linkText("Click Here"));
+        clickHereButton.click();
+
+        for (String each: driver.getWindowHandles()) {
+            driver.switchTo().window(each);
+            System.out.println("Title after clcik"+driver.getTitle());
+        }
 
 
     }
