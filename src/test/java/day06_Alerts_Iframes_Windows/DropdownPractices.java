bc87ac153d0dc1ff2ff4140e15272a2cd9de8e5f
Farid <ferid2805@gmail.com>
2023-01-12 16:00:48 +0200

    day06_ practices

---
+++ src/test/java/day06_Alerts_Iframes_Windows/DropdownPractices.java
@@ -0,0 +1,30 @@
+package day06_Alerts_Iframes_Windows;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    @Test
+    public void dropDown_Task(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/dropdown");
+
+
+        Select stateDropDown= new Select(driver.findElement(By.cssSelector("select[id='state']")));
+        stateDropDown.selectByVisibleText("Illinois");
+        stateDropDown.selectByValue("VA");
+        stateDropDown.selectByIndex(5);
+
+
+    }
+}
