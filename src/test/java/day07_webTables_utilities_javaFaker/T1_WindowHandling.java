c0525b776aaf0b48a19f786312dc17d4a94f33cf
Farid <ferid2805@gmail.com>
2023-01-14 15:55:53 +0200

    day07_ WindowsHandling

---
+++ src/test/java/day07_webTables_utilities_javaFaker/T1_WindowHandling.java
@@ -0,0 +1,24 @@
+package day07_webTables_utilities_javaFaker;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandling {
+
+    @Test
+    public void window_handling_test(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://amazon.com");
+
+    }
+
+
+
+}
