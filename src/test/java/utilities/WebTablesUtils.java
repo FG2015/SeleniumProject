e59758da104d34ffd2686137262caa110b5d6bdc
Farid <ferid2805@gmail.com>
2023-01-20 12:19:01 +0200

    Utilities Initial Commit

--- src/test/java/utilities/WebTablesUtils.java
+++ src/test/java/utilities/WebTablesUtils.java
@@ -3,6 +3,7 @@ package utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class WebTablesUtils {
 
@@ -14,6 +15,14 @@ public class WebTablesUtils {
         String locator="//td[.='Bob Martin']/following-sibling::td[3]";
         WebElement customerDateCell= driver.findElement(By.xpath(locator));
         return customerDateCell.getText();
+
+    }
+
+    public static void orderVerify(WebDriver driver, String costumerName,String expectedOrderDate){
+        String locator="//td[.='Bob Martin']/following-sibling::td[3]";
+        WebElement customerDateCell= driver.findElement(By.xpath(locator));
+        String actualOrderDate=customerDateCell.getText();
+        Assert.assertEquals(actualOrderDate,expectedOrderDate);
 
     }
 
