46aaa70934dfdf8baa6f4ac03f63d4c4b0f8a0de
Farid <ferid2805@gmail.com>
2023-01-20 16:22:44 +0200

    Configuration_Reader.java

---
+++ src/test/java/utilities/Configuration_Reader.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration_Reader {
+    private static  Properties properties=new Properties();
+
+   static {
+
+       try {
+           Properties properties=new Properties();
+
+           FileInputStream file=new FileInputStream("configuration.properties");
+           properties.load(file);
+
+           file.close();
+
+       } catch (IOException e) {
+           System.out.println("file not found in the configurationReader class");
+       }
+
+   }
+}
