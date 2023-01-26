18181b0013af985b17f0bde1c3f5543a4adf2e79
Farid <ferid2805@gmail.com>
2023-01-26 15:41:09 +0200

    initial Commit

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,36 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    private Driver(){
+    }
+    private static WebDriver driver;
+    public static WebDriver getDriver(){
+        if(driver==null){
+
+            String browserType= Configuration_Reader.getProperty("browser");
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver= new ChromeDriver();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driver.manage().window().maximize();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver= new ChromeDriver();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driver.manage().window().maximize();
+                    break;
+
+            }
+        }
+        return driver;
+
+    }
+}
