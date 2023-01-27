adf28bf4c859672c915d57920dfd78860af98d20
Farid <ferid2805@gmail.com>
2023-01-27 17:01:03 +0200

    configuration.properties

--- src/test/java/day10_upload_actions_JSexcutor/T1_registrationForm.java
+++ src/test/java/day10_upload_actions_JSexcutor/T1_registrationForm.java
@@ -1,11 +1,12 @@
 package day10_upload_actions_JSexcutor;
 
 import org.testng.annotations.Test;
+import utilities.Driver;
 
 public class T1_registrationForm {
     @Test
     public void registration_Form_Test(){
-
+        Driver.getDriver().get("");
 
     }
 }
