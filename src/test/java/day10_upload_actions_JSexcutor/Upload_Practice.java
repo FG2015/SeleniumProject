ca3168779184fe18e009fcb1f9b99c2447715a23
Farid <ferid2805@gmail.com>
2023-01-27 21:46:56 +0200

    day10 upload file test

---
+++ src/test/java/day10_upload_actions_JSexcutor/Upload_Practice.java
@@ -0,0 +1,20 @@
+package day10_upload_actions_JSexcutor;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class Upload_Practice {
+    @Test
+    public void upload_Test(){
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+        String path="C:\\Users\\User\\Desktop\\Automation\\HTML\\tomato.jpg";
+
+
+        WebElement chooseFile=Driver.getDriver().findElement(By.id("file-upload"));
+        chooseFile.sendKeys(path);
+
+
+    }
+}
