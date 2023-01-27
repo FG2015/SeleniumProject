125e56ec07956016ec1aab81bacbf6535326d795
Farid <ferid2805@gmail.com>
2023-01-27 18:57:53 +0200

    day10  registration form test

--- src/test/java/day10_upload_actions_JSexcutor/T1_registrationForm.java
+++ src/test/java/day10_upload_actions_JSexcutor/T1_registrationForm.java
@@ -1,12 +1,16 @@
 package day10_upload_actions_JSexcutor;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import utilities.Configuration_Reader;
 import utilities.Driver;
 
 public class T1_registrationForm {
     @Test
     public void registration_Form_Test(){
-        Driver.getDriver().get("");
+        Driver.getDriver().get(Configuration_Reader.getProperty("registration.form.url"));
+        WebElement inputFirstName= Driver.getDriver().findElement(By.xpath("//input[@name='firstname']"));
 
     }
 }
