bbbe6c0eff298f464cfd2e944dcf6e5a8bcc86d1
Farid <ferid2805@gmail.com>
2023-01-27 16:55:28 +0200

    day10_registration form

--- src/test/java/day09_javaFaker_testBase/practice.java
+++
@@ -1,4 +0,0 @@
-package day09_javaFaker_testBase;
-
-public class practice {
-}
---
+++ src/test/java/day10_upload_actions_JSexcutor/T1_registrationForm.java
@@ -0,0 +1,11 @@
+package day10_upload_actions_JSexcutor;
+
+import org.testng.annotations.Test;
+
+public class T1_registrationForm {
+    @Test
+    public void registration_Form_Test(){
+
+
+    }
+}
