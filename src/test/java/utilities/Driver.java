80f31401305d8cc8258e13e4308b067ac6b8b45b
Farid <ferid2805@gmail.com>
2023-01-27 17:41:48 +0200

    Driver.java

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,36 +1,58 @@
 package utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.*;
+
+import java.util.concurrent.*;
 
 public class Driver {
-    private Driver(){
-    }
-    private static WebDriver driver;
-    public static WebDriver getDriver(){
-        if(driver==null){
 
-            String browserType= Configuration_Reader.getProperty("browser");
+
+    private Driver(){}
+
+
+    private static WebDriver driver; // value is null by default
+
+
+    public static WebDriver getDriver(){
+
+        if (driver == null){
+
+
+            String browserType = Configuration_Reader.getProperty("browser");
+
+
+
             switch (browserType){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver= new ChromeDriver();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    driver = new ChromeDriver();
                     driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
-                //case "firefox":
-                  //  WebDriverManager.firefoxdriver().setup();
-                  //  driver= new ChromeDriver();
-                    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    //driver.manage().window().maximize();
-                    //break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
 
             }
         }
+
         return driver;
 
+    }
+
+    public static void closeDriver(){
+        if (driver != null){
+            driver.quit(); // this line will terminate the existing session. value will not even be null
+            driver = null;
+        }
     }
 }
