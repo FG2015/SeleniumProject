cff93b59d8eefd2ad06647657560383fef1796a6
Farid <ferid2805@gmail.com>
2023-01-31 20:25:21 +0200

    day10 Actions_Hover

---
+++ src/test/java/day10_upload_actions_JSexcutor/Actions_Hover.java
@@ -0,0 +1,13 @@
+package day10_upload_actions_JSexcutor;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class Actions_Hover {
+    @Test
+    public void hoveringTest(){
+
+        Driver.getDriver().get("https://practice.cydeo.com/hovers");
+
+    }
+}
