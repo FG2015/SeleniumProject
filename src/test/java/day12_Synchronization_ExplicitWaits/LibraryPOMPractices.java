20a1431e191e68c46a4ecf9206c0c28f623c1d78
Farid <ferid2805@gmail.com>
2023-02-10 12:20:13 +0200

    day12 LibraryPOMPractices

--- src/test/java/day12_Synchronization_ExplicitWaits/LibraryPOMPractices.java
+++ src/test/java/day12_Synchronization_ExplicitWaits/LibraryPOMPractices.java
@@ -1,5 +1,6 @@
 package day12_Synchronization_ExplicitWaits;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LibraryLoginPage;
 import utilities.Driver;
@@ -9,8 +10,21 @@ public class LibraryPOMPractices {
     public void required_field_error_msj_test(){
         Driver.getDriver().get("https://library2.cybertekschool.com/login.html");
         LibraryLoginPage libraryLoginPage=new LibraryLoginPage();
+        libraryLoginPage.signInButton.click();
+        Assert.assertTrue(libraryLoginPage.fieldRequestErrorMsj.isDisplayed());
+        Driver.closeDriver();
 
 
 
     }
+
+    @Test
+    public void wrong_email_address(){
+        Driver.getDriver().get("https://library2.cybertekschool.com/login.html");
+        LibraryLoginPage libraryLoginPage=new LibraryLoginPage();
+        libraryLoginPage.inputUserName.sendKeys("ghkgy");
+
+    }
+
+
 }
